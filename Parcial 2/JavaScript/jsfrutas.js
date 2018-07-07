c6eb1f68ea5b29bd93bc21525230f37bb5db6a62
javier efren marrufo castillo <javier.99efren@gmail.com>
2018-07-06 19:40:50 -0500

    Examen final parcial 06/07/2018

---
+++ Parcial 2/JavaScript/jsfrutas.js
@@ -0,0 +1,25 @@
+
+
+$("input[name='inputalv']").change(function(){
+
+    var inputalv = $(this).val();
+
+    switch(inputalv){
+
+        case "manzana":
+        $("#listadefrutas").html('<li>Manzana</li>');
+
+        break;
+
+        case "uva":
+
+
+
+    }
+
+
+
+
+})
+
+
